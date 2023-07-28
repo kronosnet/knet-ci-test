b716bcfd2aacccf34c615fdcbbc8eb8ba0a6e41d
Christine Caulfield <ccaulfie@redhat.com>
2023-07-28 08:32:31 +0100

    and again

--- test-helloworld.sh
+++ test-helloworld.sh
@@ -9,5 +9,12 @@ if [ "$FAILTESTS" = "yes" ]; then
 	exit 1
 fi
 
+# TEST - fail only on Debian
+if [ -f /etc/debian_version ]
+then
+    exit 1
+fi
+
+
 echo "running $build/helloworld"
 $build/helloworld
