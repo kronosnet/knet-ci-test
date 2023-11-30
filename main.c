0dc083d206328f490d38ba3d1d1992bf95ae2047
Christine Caulfield <ccaulfie@redhat.com>
2023-11-30 11:18:30 +0000

    Stop gcc errors so covscan oly finds them

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include "config.h"
 
+
+void test(int var)
+{
+	int a=var;
+	printf("test %d\n", a);
+}
+
 int main(void)
 {
 	int ret = 0;
 	int testvar;
 
-	printf("Hello World without knet: %d!\n", testvar);
+	printf("Hello World without knet\n");
+	test(testvar);
 
 	return ret;
 }
