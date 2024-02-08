3b9e62fd5efe66f17247d4c7602893041f9bdbb7
Christine Caulfield <ccaulfie@redhat.com>
2024-02-08 08:08:42 +0000

    Cause a coverity error

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include "config.h"
 
+
+void test(int var)
+{
+	int a=var;
+	printf("test %d\n", a);
+}
+
 int main(void)
 {
 	int ret = 0;
+	int testvar;
 
-	printf("Hello World without knet!\n");
+	printf("Hello World without knet\n");
+	test(testvar);
 
 	return ret;
 }
